2c20cf5547b5c27dc272eb7c92813565f717a8c7
Dustin Keller <118033913+dustinkeller@users.noreply.github.com>
2023-03-15 01:57:40 +0000

    Implemented logic to make merge sort function properly.

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ function mergeSort (array) {
     return array
   }
   // Split Array in into right and left
-
+  const length = array.length;
+  const middle = Math.floor(length / 2);
+  const left = array.slice(0,middle);
+  const right = array.slice(middle);
+  
   return merge(
     mergeSort(left),
     mergeSort(right)
@@ -13,7 +17,21 @@ function mergeSort (array) {
 }
 
 function merge(left, right){
-  
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while(leftIndex < left.length && rightIndex < right.length) {
+    if(left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  console.log("left: ", left, "right: ", right);
+  console.log("result: ", result.concat(left.slice(leftIndex).concat(right.slice(rightIndex))));
+  return result.concat(left.slice(leftIndex).concat(right.slice(rightIndex)));;
 }
 
 
